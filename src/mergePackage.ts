67748fbf6e9ead08b840cf6f6b3b5214437682ff
answershuto <answershuto@gmail.com>
2022-08-04 20:27:14 +0800

    :sparkles: feat: support mergePackage.

--- src/mergePackage.ts
+++ src/mergePackage.ts
@@ -1,5 +1,21 @@
 function mergePackage(raxPkg, icePkg) {
-  return raxPkg;
+  let pkg = {};
+  for (let key in raxPkg) {
+    let raxValue = raxPkg[key];
+    let iceValue = icePkg[key];
+    if (key === 'scripts') {
+      // Move rax scripts to ice scripts when ice scripts has't the script.
+      pkg[key] = Object.assign(raxValue, iceValue || {});
+    } else {
+      // Merge rax pkg config and ice pkg config.
+      if (Object.prototype.toString.call(raxValue) === '[Object Object]') {
+        pkg[key] = Object.assign(iceValue || {}, raxValue);
+      } else {
+        pkg[key] = raxValue;
+      }
+    }
+  }
+  return pkg;
 }
 
 export default mergePackage;
