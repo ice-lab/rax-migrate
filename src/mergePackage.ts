6717f4fab49db74d4d60e35f8884a96d9b02320a
answershuto <answershuto@gmail.com>
2022-08-09 16:59:13 +0800

    :sparkles: feat: modify merge package

--- src/mergePackage.ts
+++ src/mergePackage.ts
@@ -1,15 +1,15 @@
 function mergePackage(raxPkg, icePkg) {
-  let pkg = {};
+  let pkg = Object.assign({}, icePkg);
   for (let key in raxPkg) {
     let raxValue = raxPkg[key];
-    let iceValue = icePkg[key];
+    let iceValue = icePkg[key] || {};
     if (key === 'scripts') {
       // Move rax scripts to ice scripts when ice scripts has't the script.
-      pkg[key] = Object.assign(raxValue, iceValue || {});
+      pkg[key] = Object.assign(raxValue, iceValue);
     } else {
       // Merge rax pkg config and ice pkg config.
       if (Object.prototype.toString.call(raxValue) === '[Object Object]') {
-        pkg[key] = Object.assign(iceValue || {}, raxValue);
+        pkg[key] = Object.assign(iceValue, raxValue);
       } else {
         pkg[key] = raxValue;
       }
